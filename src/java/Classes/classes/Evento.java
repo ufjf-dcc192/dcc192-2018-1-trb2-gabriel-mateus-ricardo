3dcaa410d5bea75628ea9f3b714ab2bf48bfe823
Ricardo Alves <Ricardo Alves@DESKTOP-T6FOIFI>
2018-05-14 19:47:50 -0300

    Criando as classes Evento e Participante.

--- src/java/Classes/classes/Evento.java
+++ src/java/Classes/classes/Evento.java
@@ -1,7 +1,67 @@
 
 package Classes.classes;
 
+import java.util.Date;
+import java.util.List;
+
 
 public class Evento {
+    private String codigo;
+    private String titulo;
+    private Double minimo;
+    private Date data;
+    private Date sorteio;
+    private List<Participante> participantes;
+
+    public Evento() {
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Double getMinimo() {
+        return minimo;
+    }
+
+    public void setMinimo(Double minimo) {
+        this.minimo = minimo;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public Date getSorteio() {
+        return sorteio;
+    }
+
+    public void setSorteio(Date sorteio) {
+        this.sorteio = sorteio;
+    }
+
+    public List<Participante> getParticipantes() {
+        return participantes;
+    }
+
+    public void setParticipantes(List<Participante> participantes) {
+        this.participantes = participantes;
+    }
     
 }
---
+++ src/java/Classes/classes/Participante.java
@@ -0,0 +1,43 @@
+package Classes.classes;
+
+public class Participante {
+    private String codigo;
+    private String nome;
+    private String email;
+    private String senha;
+
+    public Participante() {
+    }
+    
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }    
+}
