342e26f466d50dd175884aadf240d6768a65a3ac
Gabriel Martins <gabrielmartinssantana@hotmail.com>
2018-05-14 20:27:52 -0300

    Criando a função listarEventos no AmigoOcultoServlet

--- src/java/amigo/oculto/AmigoOcultoServlet.java
+++ src/java/amigo/oculto/AmigoOcultoServlet.java
@@ -7,6 +7,8 @@ package amigo.oculto;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,14 +19,22 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author ice
  */
-@WebServlet(name = "AmigoOcultoServlet", urlPatterns = {"/AmigoOcultoServlet.html"})
+@WebServlet(name = "AmigoOcultoServlet", urlPatterns = {"/AmigoOcultoServlet.html", "/eventos.html"})
 public class AmigoOcultoServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      
+       if ("/eventos.html".equals(request.getServletPath())) {
+            listarEventos(request, response);
+        } 
     }
 
+    private void listarEventos(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Evento> evento = ListaEventos.getInstance();
+        request.setAttribute("evento", evento);
+        RequestDispatcher despachante = request.getRequestDispatcher("/WEB-INF/eventos.jsp");
+        despachante.forward(request, response);
+    }
 
     
 }
