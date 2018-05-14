42bbe2b1326100bcc07ad1cb080f2458e43d00a9
Mateus Gonçalo <mateusgon57@gmail.com>
2018-05-14 20:03:45 -0300

    Corrigindo SQL inicial

--- src/java/SQL/SQLInicial.sql
+++ src/java/SQL/SQLInicial.sql
@@ -1,4 +1,17 @@
 create table evento(
-    codigo INTEGER  PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    codigoEvento integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    titulo varchar(500) not null,
+    minimo decimal(10, 2) not null,
+    dataInicial date not null,
+    dataSorteio date not null
+)
+
+create table participante(
+    codigoParticipante integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    nome varchar(500) not null,
+    email varchar(500) not null,
+    senha varchar(500) not null,
+    fkid_codigoEvento integer,
+    foreign key (fkid_codigoEvento) references evento (codigoEvento)
 )
 
