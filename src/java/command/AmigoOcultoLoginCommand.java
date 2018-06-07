38bb2dfe79fd6e16cd091001264fca5eba109e67
Mateus Gonçalo <mateusgon57@gmail.com>
2018-06-07 15:59:09 -0300

    Modificando o command de amigo oculto

--- src/java/command/AmigoOcultoLoginCommand.java
+++ src/java/command/AmigoOcultoLoginCommand.java
@@ -14,12 +14,16 @@ public class AmigoOcultoLoginCommand implements Comando{
             request.setAttribute("login", true);
             Integer idUsuario = Integer.parseInt(request.getParameter("id"));
             Integer idEvento = Integer.parseInt(request.getParameter("id2"));
+            request.setAttribute("id", idUsuario);
+            request.setAttribute("id2", idEvento);
             RequestDispatcher despachante = request.getRequestDispatcher("/WEB-INF/senhaamigooculto.jsp");
             despachante.forward(request, response);
         } catch (Exception e) {
             request.setAttribute("login", false);
             Integer idUsuario = Integer.parseInt(request.getParameter("id"));
             Integer idEvento = Integer.parseInt(request.getParameter("id2"));
+            request.setAttribute("id", idUsuario);
+            request.setAttribute("id2", idEvento);
             RequestDispatcher despachante = request.getRequestDispatcher("/WEB-INF/senhaamigooculto.jsp");
             despachante.forward(request, response);
         }
